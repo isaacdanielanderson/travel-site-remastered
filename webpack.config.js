beee0cdbdb42150fb39df66b3533d6fe9dc28e04
Isaac Anderson <isaacdanielanderson@gmail.com>
2017-11-03 14:19:29 -0500

    Lec. 41 done

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 var path = require('path');
 
+// Babel
 module.exports = {
     entry: "./app/assets/scripts/App.js",
     output: {
         path: path.resolve(__dirname, "./app/temp/scripts"), // path.resolve() generates an absolute path to this directory
         filename: "App.js"
+    }, 
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                },
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
     }
 }
+// end Babel
